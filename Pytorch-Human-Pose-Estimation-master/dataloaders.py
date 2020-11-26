87ee13a0e8645e58248342bbca234a23b4c9e5f9
Megan McClure <meganemcclure@gmail.com>
2020-11-25 19:41:22 -0500

    testing something else out

--- Pytorch-Human-Pose-Estimation-master/dataloaders.py
+++ Pytorch-Human-Pose-Estimation-master/dataloaders.py
@@ -1,41 +1,15 @@
 import datasets
+import torch
 from torch.utils.data import DataLoader
 
 
-# class YourSampler(Sampler):
-#     def __init__(self, mask):
-#         self.mask = mask
-
-#     def __iter__(self):
-#         return (self.indices[i] for i in torch.nonzero(self.mask))
-
-#     def __len__(self):
-#         return len(self.mask)
-
-
-# trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-#                                         download=True, transform=transform)
-
-# sampler1 = YourSampler(your_mask)
-# sampler2 = YourSampler(your_other_mask)
-# trainloader_sampler1 = torch.utils.data.DataLoader(trainset, batch_size=4,
-#                                                    sampler=sampler1, shuffle=False, num_workers=2)
-# trainloader_sampler2 = torch.utils.data.DataLoader(trainset, batch_size=4,
-#                                                    sampler=sampler2, shuffle=False, num_workers=2)
-
-# class sampler(Sampler):
-# 	def __init__(self, mask):
-# 		self.mask = mask
-
-# 	def __iter__(self):
-# 		return (self.indices[i] for i in torch.nonzero(self.mask))
-
-# 	def __len__(self):
-# 		return len(self.mask)
-
 def ImageLoader(opts, split):
+    trainset = getattr(datasets, opts.dataset)(opts, split)
+    evens = list(range(0, len(trainset), 2))
+    trainset_1 = torch.utils.data.Subset(trainset, evens)
+
     return DataLoader(
-        dataset=getattr(datasets, opts.dataset)(opts, split)[:1000],
+        dataset=trainset_1,
         batch_size=opts.data_loader_size,
         shuffle=opts.shuffle if split == 'train' else False,
         pin_memory=not(opts.dont_pin_memory),
