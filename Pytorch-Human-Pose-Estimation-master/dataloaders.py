0772c84ac3cf3f5b17270594be2681e4adca4f29
Megan McClure <meganemcclure@gmail.com>
2020-11-25 19:57:31 -0500

    1/3 of the samples

--- Pytorch-Human-Pose-Estimation-master/dataloaders.py
+++ Pytorch-Human-Pose-Estimation-master/dataloaders.py
@@ -6,8 +6,10 @@ from torch.utils.data import DataLoader
 def ImageLoader(opts, split):
     trainset = getattr(datasets, opts.dataset)(opts, split)
     # range(start, stop, step)
-    numSamples = 10000
-    mask = list(range(0, min(numSamples, len(trainset)), 1))
+    # numSamples = 10000
+    # mask = list(range(0, min(numSamples, len(trainset)), 1))
+
+    mask = list(range(0, len(trainset), 3))  # 1/3 of the samples
     trainset_1 = torch.utils.data.Subset(trainset, mask)
 
     return DataLoader(
